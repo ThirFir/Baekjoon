9ac568c2089a34d87f6a294cd22c4c1559ee888e
Thirfir <sangbbang31@gmail.com>
2022-03-30 15:00:39 +0900

    silver 3

---
+++ 11726.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int dp[1001];
+
+int solve(int n) {
+	if (n <= 1)
+		return 1;
+	int& ret = dp[n];
+	//if (ret != -1) return ret;
+
+	ret = (solve(n - 1) + solve(n - 2)) % 10007;
+	return ret;
+}
+int main() {  
+	memset(dp, -1, sizeof(int) * 1001);
+	int n; cin >> n;
+	cout << solve(n);
+}
